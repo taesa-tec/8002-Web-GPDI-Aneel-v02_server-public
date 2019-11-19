7a1f21b4bd578b915b0a81997252bfdd62db7f86
Diego França <diego.franca@lojainterativa.com>
2019-11-19 15:32:03 -0300

    Cargo de usuários

---
+++ Migrations/20191119175944_UserCargo.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace APIGestor.Migrations
+{
+    public partial class UserCargo : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Cargo",
+                table: "AspNetUsers",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Cargo",
+                table: "AspNetUsers");
+        }
+    }
+}
