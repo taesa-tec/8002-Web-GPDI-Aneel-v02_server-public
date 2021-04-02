c3c8ace1337e24c9c906e4d780484a876b97b6b3
Diego França <diegofranca90@gmail.com>
2021-04-02 10:50:05 -0300

    Somente um produto final por proposta

--- PeD/Controllers/Fornecedores/Propostas/ProdutosController.cs
+++ PeD/Controllers/Fornecedores/Propostas/ProdutosController.cs
@@ -3,12 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using AutoMapper;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using PeD.Core.ApiModels.Propostas;
 using PeD.Core.Models;
 using PeD.Core.Models.Propostas;
 using PeD.Core.Requests.Proposta;
+using PeD.Data;
 using PeD.Services.Captacoes;
 using Swashbuckle.AspNetCore.Annotations;
 using TaesaCore.Controllers;
@@ -59,20 +61,29 @@ namespace PeD.Controllers.Fornecedores.Propostas
         }
 
         [HttpPost]
-        public ActionResult Post([FromRoute] int captacaoId, [FromBody] PropostaProdutoRequest request)
+        public ActionResult Post([FromRoute] int captacaoId, [FromBody] PropostaProdutoRequest request,
+            [FromServices] GestorDbContext context)
         {
             var proposta = _propostaService.GetPropostaPorResponsavel(captacaoId, this.UserId());
             var produto = Mapper.Map<Produto>(request);
             produto.PropostaId = proposta.Id;
             produto.Created = DateTime.Now;
-            Service.Post(produto);
-            _propostaService.UpdatePropostaDataAlteracao(proposta.Id);
-            return Ok();
+            if (produto.Classificacao != ProdutoClassificacao.Final || !context.Set<Produto>()
+                .Any(p => p.Classificacao == ProdutoClassificacao.Final && p.PropostaId == proposta.Id))
+            {
+                Service.Post(produto);
+
+                _propostaService.UpdatePropostaDataAlteracao(proposta.Id);
+                return Ok();
+            }
+
+            return Problem("Somente um produto final por proposta", null, StatusCodes.Status409Conflict);
         }
 
         [HttpPut]
         public IActionResult Put([FromRoute] int captacaoId,
-            [FromBody] PropostaProdutoRequest request)
+            [FromBody] PropostaProdutoRequest request,
+            [FromServices] GestorDbContext context)
         {
             var proposta = _propostaService.GetPropostaPorResponsavel(captacaoId, this.UserId());
             var produtoPrev = Service
@@ -85,9 +96,16 @@ namespace PeD.Controllers.Fornecedores.Propostas
             produto.PropostaId = proposta.Id;
             produto.Created = produtoPrev.Created;
 
-            Service.Put(produto);
-            _propostaService.UpdatePropostaDataAlteracao(proposta.Id);
-            return Ok(Mapper.Map<PropostaProdutoDto>(produto));
+            if (produto.Classificacao != ProdutoClassificacao.Final || !context.Set<Produto>()
+                .Any(p => p.Classificacao == ProdutoClassificacao.Final && p.PropostaId == proposta.Id &&
+                          p.Id != produto.Id))
+            {
+                Service.Put(produto);
+                _propostaService.UpdatePropostaDataAlteracao(proposta.Id);
+                return Ok(Mapper.Map<PropostaProdutoDto>(produto));
+            }
+
+            return Problem("Somente um produto final por proposta", null, StatusCodes.Status409Conflict);
         }
 
         [HttpDelete]
