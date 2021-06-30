edab6c91cfab491bd642c59d8be116a98da81f03
Diego França <diegofranca90@gmail.com>
2021-06-30 11:11:32 -0300

    Dto Tipo socioambiental corrigido

--- PeD.Core/ApiModels/Projetos/Resultados/SocioambientalDto.cs
+++ PeD.Core/ApiModels/Projetos/Resultados/SocioambientalDto.cs
@@ -2,15 +2,7 @@ namespace PeD.Core.ApiModels.Projetos.Resultados
 {
     public class SocioambientalDto : ProjetoNodeDto
     {
-        public enum TipoIndicador
-        {
-            ISA1,
-            ISA2,
-            ISA3,
-            ISA4
-        }
-
-        public TipoIndicador Tipo { get; set; }
+        public string Tipo { get; set; }
         public bool ResultadoPositivo { get; set; }
         public string DescricaoResultado { get; set; }
     }
