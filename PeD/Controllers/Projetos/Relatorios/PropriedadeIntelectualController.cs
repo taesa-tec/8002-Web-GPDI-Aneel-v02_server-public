96d44a1f865fd066c2f495f8e49e938a7eadccf7
Diego França <diego.franca@lojainterativa.com>
2021-07-05 11:52:13 -0300

    Includes Propriedade Intelectual

--- PeD/Controllers/Projetos/Relatorios/PropriedadeIntelectualController.cs
+++ PeD/Controllers/Projetos/Relatorios/PropriedadeIntelectualController.cs
@@ -1,9 +1,12 @@
+using System.Linq;
 using AutoMapper;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using PeD.Core.ApiModels.Projetos.Resultados;
 using PeD.Core.Models.Projetos.Resultados;
 using PeD.Core.Requests.Projetos.Resultados;
+using PeD.Data;
 using PeD.Services.Projetos;
 using TaesaCore.Interfaces;
 
@@ -15,10 +18,39 @@ namespace PeD.Controllers.Projetos.Relatorios
     public class PropriedadeIntelectualController : ProjetoNodeBaseController<PropriedadeIntelectual,
         PropriedadeIntelectualRequest, PropriedadeIntelectualDto>
     {
+        private GestorDbContext _context;
+
         public PropriedadeIntelectualController(IService<PropriedadeIntelectual> service, IMapper mapper,
-            IAuthorizationService authorizationService, ProjetoService projetoService) : base(service, mapper,
+            IAuthorizationService authorizationService, ProjetoService projetoService, GestorDbContext context) : base(
+            service, mapper,
             authorizationService, projetoService)
         {
+            _context = context;
+        }
+
+        protected override IQueryable<PropriedadeIntelectual> Includes(IQueryable<PropriedadeIntelectual> queryable)
+        {
+            return queryable
+                .Include(p => p.Depositantes)
+                .ThenInclude(d => d.Empresa)
+                .Include(p => p.Depositantes)
+                .ThenInclude(d => d.CoExecutor)
+                .Include(d => d.Inventores)
+                .ThenInclude(i => i.Recurso);
+        }
+
+        protected override void BeforePut(PropriedadeIntelectual actual, PropriedadeIntelectual @new)
+        {
+            var depositantes = _context.Set<PropriedadeIntelectualDepositante>()
+                .Where(p => p.PropriedadeId == actual.Id).ToList();
+            var invetores = _context.Set<PropriedadeIntelectualInventor>()
+                .Where(p => p.PropriedadeId == actual.Id).ToList();
+            _context.RemoveRange(depositantes);
+            _context.RemoveRange(invetores);
+            _context.SaveChanges();
+            _context.AddRange(@new.Depositantes);
+            _context.AddRange(@new.Inventores);
+            _context.SaveChanges();
         }
     }
 }
